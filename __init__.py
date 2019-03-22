75b93f890a587ceed4ed165fa6b94c3584740f9d
anigmo97 <angeltecnolo@gmail.com>
2019-03-22 14:02:28 +0100

    first graph added

--- __init__.py
+++ __init__.py
@@ -52,6 +52,20 @@ def aboutcontactpage():
 	except Exception as e:
 		return str(e)
 
+@app.route('/graph_example')
+def graph(chartID = 'primer_chart', chart_type = 'line', chart_height = 500):
+	webpage_title = "EPIC TITLE"
+	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
+	series = [{"name": 'Label1', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
+	title = {"text": 'My Title'}
+	xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
+	yAxis = {"title": {"text": 'yAxis Label'}}
+	return render_template('index_graph.html', webpage_title=webpage_title,chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
+ 
+ 
+
+
+
 @app.route('/stop')
 def stop():
 	global run_flag
