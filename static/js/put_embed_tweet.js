24d208fc896a72efe5c56a65f7e2557f9f81c6ee
anigmo97 <angeltecnolo@gmail.com>
2019-03-24 11:11:18 +0100

    first div delay

--- static/js/put_embed_tweet.js
+++ static/js/put_embed_tweet.js
@@ -3,10 +3,10 @@ function put_embed_tweet(embed_tweet_dict, with_media) {
      //alert("with media "+with_media);
      if (with_media){
         //alert(embed_tweet_dict["embed_with_media"]);
-        var content = embed_tweet_dict["embed_with_media"]
+        var content = '<div>'+ embed_tweet_dict["embed_with_media"]+'</div>'
      } else {
         //alert(embed_tweet_dict["embed_without_media"]);
-        var content = embed_tweet_dict["embed_without_media"]
+        var content = '<div>'+ embed_tweet_dict["embed_without_media"]+'</div>'
      }
      document.write(content)
 }
