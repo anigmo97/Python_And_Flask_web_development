20aadf4f61b1bccce8d8bc25fcfb5811d6613ecf
anigmo97 <angeltecnolo@gmail.com>
2019-03-24 09:10:58 +0100

    tweets embed correctly

--- static/js/put_embed_tweet.js
+++ static/js/put_embed_tweet.js
@@ -1,5 +1,5 @@
 function put_embed_tweet(embed_tweet_dict, with_media) {
-     var str = JSON.stringify(embed_dict, null, 2);
+     //var str = JSON.stringify(embed_dict, null, 2);
      //alert("with media "+with_media);
      if (with_media){
         //alert(embed_tweet_dict["embed_with_media"]);
